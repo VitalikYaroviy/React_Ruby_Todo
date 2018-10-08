2faeacb438d8d83be5d53521c0c1f341a4dc4304
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-08 14:27:33 +0300

    ready for first push to heroku

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module ListApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:3000', 'https://thawing-eyrie-58542.herokuapp.com/'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
