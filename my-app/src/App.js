815ede1b341dc1bd0912e7f2e6512ab3c490d9b6
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-17 17:37:05 +0300

    ready for first push to heroku

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,17 @@ import Signin from './component/Signin/Signin'
 import Registration from './component/Registration/Registration'
 import Tasks from './component/Task/Tasks'
 import Confirm_email from './component/Confirm_email/Confirm_email'
-import { HashRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HashRouter>
+        <Router>
             <Route exact path='/' component={Signin} />
-            <Route exact path='/user/confirm_email/' component={Confirm_email}/>
-            <Route exact path='/registration' component={Registration} />
-            <Route exact path='/tasks' component={Tasks} />
-        </HashRouter>
+            <Route  path='/user/confirm_email/' component={Confirm_email}/>
+            <Route  path='/registration' component={Registration} />
+            <Route  path='/tasks' component={Tasks} />
+        </Router>
       </div>
     );
   }
