d750ba33ff32340040d65177cd59ce2123971c26
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-17 15:40:56 +0300

    ready for first push to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4000/'
+    origins %w[
+        https://example.com
+         http://example.com
+        https://www.example.com
+         http://www.example.com
+        https://example.herokuapp.com
+         http://example.herokuapp.com
+      ]
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
