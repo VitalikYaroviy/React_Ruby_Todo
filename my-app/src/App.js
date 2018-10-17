8331fa1f39b736a7c560b8d915b743863af4ed32
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-17 17:21:36 +0300

    ready for first push to heroku

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, browserHistory} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import Signin from './component/Signin/Signin'
 import Registration from './component/Registration/Registration'
 import Tasks from './component/Task/Tasks'
 import Confirm_email from './component/Confirm_email/Confirm_email'
+import { HashRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router history={browserHistory}>
+        <HashRouter>
             <Route exact path='/' component={Signin} />
             <Route exact path='/user/confirm_email/' component={Confirm_email}/>
             <Route exact path='/registration' component={Registration} />
             <Route exact path='/tasks' component={Tasks} />
-        </Router>
+        </HashRouter>
       </div>
     );
   }
