a6a3ee8da02115396d4e59ae3b98455981bf2dac
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-18 13:34:12 +0300

    Start with create-react-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,19 +5,21 @@ import Signin from './component/Signin/Signin'
 import Registration from './component/Registration/Registration'
 import Tasks from './component/Task/Tasks'
 import Confirm_email from './component/Confirm_email/Confirm_email'
+import { BrowserRouter } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <BrowserRouter>
           <div>
             <Route exact path='/' component={Signin} />
             <Route path='/user/confirm_email/' component={Confirm_email}/>
             <Route path='/registration' component={Registration} />
             <Route path='/tasks' component={Tasks} />
           </div>
-        </Router>
+        </BrowserRouter>
       </div>
     );
   }
