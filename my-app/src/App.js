b5dcf785c240a3e0cb894355ea8da9703a12582c
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-18 13:05:17 +0300

    ready for first push to heroku

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
         <Router>
           <div>
             <Route exact path='/' component={Signin} />
-            <Route exact path='/user/confirm_email/' component={Confirm_email}/>
-            <Route exact path='/registration' component={Registration} />
-            <Route exact path='/tasks' component={Tasks} />
+            <Route path='/user/confirm_email/' component={Confirm_email}/>
+            <Route path='/registration' component={Registration} />
+            <Route path='/tasks' component={Tasks} />
           </div>
         </Router>
       </div>
