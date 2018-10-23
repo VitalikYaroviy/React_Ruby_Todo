7a2c6ae8b711abc197ee2a68994a28a6e42bc7d0
VitalikYaroviy <vitalya.yaroviy97@gmail.com>
2018-10-23 11:07:04 +0300

    added oorkeeper_authorize to tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  # before_action :doorkeeper_authorize!
+  before_action :doorkeeper_authorize!
   before_action :set_task, only: [:show, :edit]
   skip_before_action :verify_authenticity_token
 
